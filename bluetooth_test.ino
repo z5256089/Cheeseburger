f56b59e89ce4545ed576308cc93d6ac27355f3c0
rxwsym <134828465+rxwsym@users.noreply.github.com>
2023-07-04 13:22:40 +1000

    Update bluetooth.ino

--- bluetooth_test.ino
+++ bluetooth_test.ino
@@ -1,23 +1,17 @@
 #include <Arduino.h>
 
-#define Pin14  14
-#define Pin15  15
-char character;
-
 void setup() { 
   Serial.begin(9600);
-  pinMode(Pin14, OUTPUT); 
-  pinMode(Pin15, OUTPUT); 
+  Serial3.begin(9600);
 }
 
 void loop() { 
   if(Serial.available()) {
-  character = Serial.read();
+    Serial3.write(Serial.read());
+  }
   
-    if(character == 'a'){ digitalWrite(Pin14, HIGH);}
-    if(character == 'b'){ digitalWrite(Pin14, LOW);} 
-    if(character == 'c'){ digitalWrite(Pin15, HIGH);}
-    if(character == 'd'){ digitalWrite(Pin15, LOW);}
-  } 
-
+  if(Serial3.available()) {
+    Serial.write(Serial3.read());
+    Serial.println(Serial3.readString());
+  }
 } 
